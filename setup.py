852577cd688dd4caaf1ce3565404e8d971f0d3ed
tevans <tom.evans@trinetcloud.com>
2017-06-05 15:07:42 -0500

    Working with setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SaleMail',
+      version='1.0',
+	  description='Mass Email Application',
+	  author='Thomas Evans',
+	  author_email='tsevans@vt.edu',
+	  url='https://github.com/tsevans/SaleMail',
+	  py_modules=['xlrd'])
